268df767ec1337e1eab7ae51017a234a0d247d70
Thomas-Geslin <98038413+Thomas-Geslin@users.noreply.github.com>
2022-04-02 22:08:18 +0200

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 let db = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'Oc0604love*',
+    password : 'password',
     database : 'groupomania'
 });
   
